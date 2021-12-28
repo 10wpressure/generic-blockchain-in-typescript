4d9a5e8e2074171cd1a3f093cbb5b3c56078f236
Boris Kuznetsov <boris.kuznetsov@latoken.com>
2021-12-28 10:06:33 +0300

    broadcast tested

--- dev/networkNode.ts
+++ dev/networkNode.ts
@@ -22,8 +22,9 @@ app.get('/blockchain', (req, res) => {
 
 // add transaction to blockchain
 app.post('/transaction', (req, res) => {
-    const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
-    res.json({ note: `Transaction will be added in block ${blockIndex}.` });
+    const newTransaction = req.body;
+    const blockIndex = bitcoin.addTransactionToPendingTransactions(newTransaction);
+    res.json({ note: `Transaction will be added in block ${blockIndex}.`})
 });
 
 app.post('/transaction/broadcast', (req, res) => {
